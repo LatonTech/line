25b16582f0ae2d366c9e2a67c6586a15586f6435
LatonTech <77439943+LatonTech@users.noreply.github.com>
2023-03-05 15:22:58 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let LeftLineSensor = 0
+let RightLineSensor = 0
+basic.forever(function () {
+    LeftLineSensor = bitbot.readLine(BBLineSensor.Left)
+    RightLineSensor = bitbot.readLine(BBLineSensor.Right)
+    if (LeftLineSensor == (false && 0 == 0)) {
+    	
+    } else {
+    	
+    }
+})
